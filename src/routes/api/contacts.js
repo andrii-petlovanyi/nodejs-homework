f1d51f30618217757bd9e076f9becea43f6f3346
Andrii Petlovanyi <33178699+andrii-petlovanyi@users.noreply.github.com>
2023-01-10 17:32:30 +0200

    fixed(Routes contacts): optimise code in routes for contacts

--- src/routes/api/contacts.js
+++ src/routes/api/contacts.js
@@ -18,40 +18,19 @@ import { contactSchema, favoriteSchema } from "../../schemas/contactSchema.js";
 
 const router = new Router();
 
-router.get("/", checkJWTAuth, ctrlWrapper(getAllContCtrl));
-
-router.get(
-  "/:contactId",
-  checkJWTAuth,
-  isValidId,
-  ctrlWrapper(getContByIdCtrl)
-);
-
-router.post(
-  "/",
-  checkJWTAuth,
-  reqValidation(contactSchema),
-  ctrlWrapper(addContCtrl)
-);
-
-router.delete(
-  "/:contactId",
-  checkJWTAuth,
-  isValidId,
-  ctrlWrapper(removeContByIdCtrl)
-);
-
+router.use(checkJWTAuth);
+router.get("/", ctrlWrapper(getAllContCtrl));
+router.get("/:contactId", isValidId, ctrlWrapper(getContByIdCtrl));
+router.post("/", reqValidation(contactSchema), ctrlWrapper(addContCtrl));
+router.delete("/:contactId", isValidId, ctrlWrapper(removeContByIdCtrl));
 router.put(
   "/:contactId",
-  checkJWTAuth,
   isValidId,
   reqValidation(contactSchema),
   ctrlWrapper(updateContByIdCtrl)
 );
-
 router.patch(
   "/:contactId/favorite",
-  checkJWTAuth,
   isValidId,
   reqValidation(favoriteSchema),
   ctrlWrapper(updateStatusContactCtrl)
